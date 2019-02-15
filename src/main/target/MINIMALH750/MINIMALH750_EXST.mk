77cf95947987bd3aae2099b82789a49e4bfda41a
Dominic Clifton <dominic.clifton@cleanflight.com>
2019-02-15 19:17:54 +0100

    MINIMALH750 - Add minimal H750 target.

---
+++ src/main/target/MINIMALH750/MINIMALH750_EXST.mk
@@ -0,0 +1,6 @@
+FEATURES       += VCP ONBOARDFLASH SDCARD_SDIO
+
+TARGET_SRC += \
+            drivers/accgyro/accgyro_fake.c \
+            drivers/barometer/barometer_fake.c \
+            drivers/compass/compass_fake.c \
---
+++ src/main/target/MINIMALH750/target.c
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Cleanflight and Betaflight.
+ *
+ * Cleanflight and Betaflight are free software. You can redistribute
+ * this software and/or modify this software under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Cleanflight and Betaflight are distributed in the hope that they
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdint.h>
+
+#include "platform.h"
+#include "drivers/io.h"
+
+#include "drivers/dma.h"
+#include "drivers/timer.h"
+#include "drivers/timer_def.h"
+
+const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
+    DEF_TIM(TIM5,  CH1, PA0,  TIM_USE_MOTOR,               0,  0,  0 ), // M1
+    DEF_TIM(TIM5,  CH2, PA1,  TIM_USE_MOTOR,               0,  0,  0 ),
+    DEF_TIM(TIM5,  CH3, PA2,  TIM_USE_MOTOR,               0,  0,  0 ),
+    DEF_TIM(TIM5,  CH4, PA3,  TIM_USE_MOTOR,               0,  0,  0 ), // M4
+};
+
+#if defined(EEPROM_IN_RAM)
+PERSISTENT uint8_t eepromData[EEPROM_SIZE]; // persistent, so it survives warm boots.
+#elif defined (EEPROM_IN_EXTERNAL_FLASH)
+uint8_t eepromData[EEPROM_SIZE];
+#endif
